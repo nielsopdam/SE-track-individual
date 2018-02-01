647fea3525282db7dd906336c39cfa32139da41f
CORP\nopdam <niels.opdam@capgemini.com>
2018-02-01 20:18:59 +0100

    Added queries to find specific flights

--- src/main/java/com/capgemini/setrack/repository/FlightRepository.java
+++ src/main/java/com/capgemini/setrack/repository/FlightRepository.java
@@ -1,7 +1,30 @@
 package com.capgemini.setrack.repository;
 
 import com.capgemini.setrack.model.Flight;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface FlightRepository extends CrudRepository<Flight, Long> {
+    @Query("SELECT f FROM Flight f WHERE f.origin.id = :origin AND f.liftOffTime BETWEEN :from AND :to")
+    Iterable<Flight> findDepartures(@Param("origin") long destination,
+                                                  @Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
+
+    @Query("SELECT f FROM Flight f WHERE f.origin.id = :origin")
+    Iterable<Flight> findDepartures(@Param("origin") long destination);
+
+    @Query("SELECT f FROM Flight f WHERE f.origin.id = :origin AND f.liftOffTime > :from")
+    Iterable<Flight> findDepartures(@Param("origin") long destination, @Param("from") LocalDateTime from);
+
+    @Query("SELECT f FROM Flight f WHERE f.destination.id = :destination AND f.liftOffTime BETWEEN :from AND :to")
+    Iterable<Flight> findArrivals(@Param("destination") long destination,
+                                    @Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
+
+    @Query("SELECT f FROM Flight f WHERE f.destination.id = :destination")
+    Iterable<Flight> findArrivals(@Param("destination") long destination);
+
+    @Query("SELECT f FROM Flight f WHERE f.destination.id = :destination AND f.liftOffTime > :from")
+    Iterable<Flight> findArrivals(@Param("destination") long destination, @Param("from") LocalDateTime from);
 }
