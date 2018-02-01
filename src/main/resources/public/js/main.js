ea269e5eb962a32c67d6df4db25f2a6924865808
CORP\nopdam <niels.opdam@capgemini.com>
2018-02-01 20:18:18 +0100

    Created main js file with common functions

---
+++ src/main/resources/public/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    setAirports();
+});
+
+function setAirports(){
+    ajaxJsonCall('GET', '/api/airports/', null, function(airports) {
+          airports.forEach(function(airport) {
+                console.log(airport);
+                $("#nav-airports").append('<li><a href="/flights.html?airport=' + airport.id + '&type=0">' + airport.city + '</a></li>');
+          })}
+          , null)
+}
+
+function countdown(date, flight_nr){
+var countDownDate = new Date(date).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown_" + flight_nr).innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown_" + flight_nr).innerHTML = "Taken Off";
+  }
+}, 1000);
+}
