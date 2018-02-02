f5037b44529db098d6d20cd56bdf1656ef93f7b5
CORP\nopdam <niels.opdam@capgemini.com>
2018-02-02 16:18:26 +0100

    Added a way to calculate distance between two cities

---
+++ src/main/java/com/capgemini/setrack/utility/APIDistanceCalculator.java
@@ -0,0 +1,32 @@
+package com.capgemini.setrack.utility;
+
+import org.json.JSONObject;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class APIDistanceCalculator implements DistanceCalculator {
+
+    public int getDistanceBetweenCities(String origin, String destination) throws Exception {
+        Client client = ClientBuilder.newClient();
+
+        WebTarget resource = client.target("http://nl.afstand.org/route.json?stops=" + origin + "%7C" + destination);
+
+        Invocation.Builder request = resource.request();
+        request.accept(MediaType.APPLICATION_JSON);
+
+        Response response = request.get();
+
+        if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
+            String responseString = response.readEntity(String.class);
+            JSONObject jsonObject = new JSONObject(responseString);
+            return jsonObject.getInt("distance");
+        } else {
+            throw new Exception("Could not get the distance!");
+        }
+    }
+}
---
+++ src/main/java/com/capgemini/setrack/utility/DistanceCalculator.java
@@ -0,0 +1,6 @@
+package com.capgemini.setrack.utility;
+
+public interface DistanceCalculator {
+
+    public int getDistanceBetweenCities(String origin, String destination) throws Exception;
+}
